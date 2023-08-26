84067c05e6bb251d3d57ddffddf8cfe4cdb5c0b5
JRendel-main <mrskybrine@gmail.com>
2023-08-26 12:16:53 +0800

    added cor to registarion

--- register.php
+++ register.php
@@ -2,3 +2,4 @@
 include_once 'includes/header.html';
 include_once 'includes/registration_page.html';
 include_once 'includes/footer.html';
+include_once 'includes/register-footer.php';
--- server/tutee/get-tutorprofile.php
+++ server/tutee/get-tutorprofile.php
@@ -4,81 +4,15 @@ require_once '../db-connect.php';
 $peer_id = $_POST['peer_id'];
 
 // get the tutor profile
-$sql = "
-    SELECT
-        a.firstname,
-        a.middlename,
-        a.lastname,
-        a.email,
-        a.course,
-        COALESCE(b.bio, 'No bio available') AS bio,
-        COALESCE(b.about_me, 'No about me information') AS about_me,
-        c.title,
-        c.description,
-        c.place,
-        c.start,
-        c.duration,
-        c.date,
-        c.max_tutee
-    FROM
-        tbl_peerinfo a
-    JOIN
-        tbl_schedules c ON a.peer_id = c.peer_id
-    LEFT JOIN
-        tbl_tutor_profile b ON a.peer_id = b.peer_id
-    WHERE
-        a.peer_id = $peer_id;
-";
+$sql = "SELECT * FROM tbl_peerinfo WHERE peer_id = $peer_id";
 
+$sql2 = "SELECT * FROM tbl_tutor_profile WHERE peer_id = $peer_id";
 
-$sql2 = "SELECT bio, about_me FROM tbl_tutor_profile WHERE peer_id = $peer_id";
+$sql3 = "SELECT * FROM tbl_schedules WHERE peer_id = $peer_id";
 
-$result = $conn->query($sql);
-if ($result) {
-    $row = $result->fetch_assoc();
-    $result2 = $conn->query($sql2);
-    $row2 = $result2->fetch_assoc();
-    $firstname = $row['firstname'];
-    $middlename = $row['middlename'];
-    $lastname = $row['lastname'];
-    $email = $row['email'];
-    $title = $row['title'];
-    $bio = $row2['bio'];
-    $about_me = $row2['about_me'];
-    $department = $row['course'];
-    $description = $row['description'];
-    $place = $row['place'];
-    $start = $row['start'];
-    $duration = $row['duration'];
-    $date = $row['date'];
-    $max_tutee = $row['max_tutee'];
-    $fullname = $firstname . ' ' . $middlename . ' ' . $lastname;
-    $tutor = array(
-        'fullname' => $fullname,
-        'email' => $email,
-        'department' => $department,
-        'bio' => $bio,
-        'about' => $about_me,
-        'title' => $title,
-        'description' => $description,
-        'place' => $place,
-        'start' => $start,
-        'duration' => $duration,
-        'date' => $date,
-        'max_tutee' => $max_tutee,
-        'status' => 1
-    );
-    $response = array(
-        'success' => true,
-        'tutor' => $tutor
-    );
-
-} else {
-    $response = array(
-        'success' => false,
-        'message' => 'Error: ' . $conn->error
-    );
-}
+$result = mysqli_query($conn, $sql);
+$result2 = mysqli_query($conn, $sql2);
+$result3 = mysqli_query($conn, $sql3);
 // send back the json to ajax
 echo json_encode($response);
 
